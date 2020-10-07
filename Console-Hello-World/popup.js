a8cdc5591bebd84e52fe0038a60c10023d8fe117
Gabriel Tellaroli Ramos <gabriel.tellaroli@dp6inc.local>
2020-10-07 14:46:49 -0300

    exclude table_title.

--- Console-Hello-World/popup.js
+++ Console-Hello-World/popup.js
@@ -5,7 +5,6 @@ function display_h1 (results){
 }
 chrome.tabs.query({active: true}, function(tabs) {
   let tab = tabs[0];
-  tab_title = tab.title;
   chrome.tabs.executeScript(tab.id, {
     file: './teste.js'
   }, display_h1);
