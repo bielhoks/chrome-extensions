132492070f70bc5de8ad63c683f9943c02fa7502
Gabriel Tellaroli Ramos <gabriel.tellaroli@dp6inc.local>
2020-10-16 15:46:37 -0300

    Refactor(teste.js): É possivel modificar o window.

--- Console-Hello-World/popup.js
+++ Console-Hello-World/popup.js
@@ -1,8 +1,8 @@
-
+// Não está sendo usada a function pois o script não retorna nada.
 function display_h1 (results){
-  url=results;
-  document.querySelector("#id1").innerHTML = "<p>Page Url: " + url ;
+  console.log(results);
 }
+
 chrome.tabs.query({active: true}, function(tabs) {
   let tab = tabs[0];
   chrome.tabs.executeScript(tab.id, {
--- Console-Hello-World/teste.js
+++ Console-Hello-World/teste.js
@@ -1,5 +1,5 @@
-console.log('Hellow World')
-let getUrl = () =>{
-    return window.origin;
-}
-getUrl();
+// Cria um elemento do tipo script no html da pagina, e esse script modifica o objeto window.
+
+var script = document.createElement("script");
+script.innerHTML = "window.bowserTeste = { text: 'Hello'}";
+document.head.appendChild(script);
